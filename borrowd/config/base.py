8b39eea752ce1203ef55d915fc1db94985a9382f
Ryan Jendoubi <ryan.jendoubi@gmail.com>
2025-04-13 16:56:22 +0100

    Recreate landing page from Softr MVP.
    Using Tailwind instead of Bootstrap.
    
    Using django-cotton to factor out components. Need to make better,
    more extensive use of it going forward.

--- borrowd/config/base.py
+++ borrowd/config/base.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_cotton",
     "borrowd_users",  # Must be above `allauth` to use our templates
     "allauth",
     "allauth.account",
