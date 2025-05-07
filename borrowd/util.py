borrowd/borrowd

commit a82b24e22e1a3c2179d39fe17d327f2a081f0aee

Rename custom Group to `BorrowdGroup`.
It was maybe naively ambitious to think I could just "sneak in" a
custom group with the same name as the default `auth.Group` class.

In practice, this caused ambiguity the very first time I opened up
a Django shell: `g = Group(name="foo")` created an `auth.Group`,
not our custom Group. Maybe that could have been overcome with
different app ordering, but it created a great sense of foreboding
of future confusion that this could cause, and spooked me into doing
the more reasonable thing I should have done from the start, i.e.,
use a distinct name for the custom group class. This will make things
much more obvious in future, e.g. if a third party lib wants to work
with "the group object", it'll be clearer up-front if it's using the
wrong one.

Note `BorrowdGroup` still subclasses `auth.Group`, as it did before.

borrowd/util.py
@@ -1,6 +1,6 @@
 from django.db.models import Model
 
-from borrowd_groups.models import Group
+from borrowd_groups.models import BorrowdGroup
 
 
 class BorrowdTemplateFinderMixin:
@@ -22,7 +22,7 @@ class BorrowdTemplateFinderMixin:
     mixin so that we can use it generically across all of our apps.
     """
 
-    model: type[Model | Group]
+    model: type[Model | BorrowdGroup]
     template_name_suffix: str
 
     def get_template_names(self) -> list[str]:

borrowd_groups/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 
-from .models import Group
+from .models import BorrowdGroup
 
-admin.site.register(Group)
+admin.site.register(BorrowdGroup)

borrowd_groups/migrations/0002_rename_group_borrowdgroup.py
@@ -1,0 +1,17 @@
+# Generated by Django 5.2 on 2025-04-29 09:39
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("auth", "0012_alter_user_first_name_max_length"),
+        ("borrowd_groups", "0001_initial"),
+    ]
+
+    operations = [
+        migrations.RenameModel(
+            old_name="Group",
+            new_name="BorrowdGroup",
+        ),
+    ]

borrowd_groups/models.py
@@ -1,6 +1,6 @@
 from typing import Never  # Unfortunately needed for more mypy shenanigans
 
-from django.contrib.auth.models import Group as DefaultGroup
+from django.contrib.auth.models import Group
 from django.db.models import (
     BooleanField,
     TextField,
@@ -8,7 +8,7 @@ from django.db.models import (
 from django.urls import reverse
 
 
-class Group(DefaultGroup):
+class BorrowdGroup(Group):
     """
     A group of users. This is a subclass of Django's built-in Group
     model. There is no clean and widely-accepted way of using a

borrowd_groups/views.py
@@ -10,11 +10,13 @@ from django.views.generic import (
 
 from borrowd.util import BorrowdTemplateFinderMixin
 
-from .models import Group
+from .models import BorrowdGroup
 
 
-class GroupCreateView(BorrowdTemplateFinderMixin, CreateView[Group, ModelForm[Group]]):
-    model = Group
+class GroupCreateView(
+    BorrowdTemplateFinderMixin, CreateView[BorrowdGroup, ModelForm[BorrowdGroup]]
+):
+    model = BorrowdGroup
     fields = ["name", "description", "membership_requires_approval"]
 
     def get_success_url(self) -> str:
@@ -23,21 +25,25 @@ class GroupCreateView(BorrowdTemplateFinderMixin, CreateView[Group, ModelForm[Gr
         return reverse("borrowd_groups:group-detail", args=[self.object.pk])
 
 
-class GroupDeleteView(BorrowdTemplateFinderMixin, DeleteView[Group, ModelForm[Group]]):
-    model = Group
+class GroupDeleteView(
+    BorrowdTemplateFinderMixin, DeleteView[BorrowdGroup, ModelForm[BorrowdGroup]]
+):
+    model = BorrowdGroup
     success_url = reverse_lazy("borrowd_groups:group-list")
 
 
-class GroupDetailView(BorrowdTemplateFinderMixin, DetailView[Group]):
-    model = Group
+class GroupDetailView(BorrowdTemplateFinderMixin, DetailView[BorrowdGroup]):
+    model = BorrowdGroup
 
 
-class GroupListView(BorrowdTemplateFinderMixin, ListView[Group]):
-    model = Group
+class GroupListView(BorrowdTemplateFinderMixin, ListView[BorrowdGroup]):
+    model = BorrowdGroup
 
 
-class GroupUpdateView(BorrowdTemplateFinderMixin, UpdateView[Group, ModelForm[Group]]):
-    model = Group
+class GroupUpdateView(
+    BorrowdTemplateFinderMixin, UpdateView[BorrowdGroup, ModelForm[BorrowdGroup]]
+):
+    model = BorrowdGroup
     fields = ["name", "description", "membership_requires_approval"]
 
     def get_success_url(self) -> str:

commit e926cfa576a77e76656c9dfe9ae9d08bb25abe72

Fix lookup of BorrowdGroup template names.
It's verbose to have to type "borrowdgroup_foo" for all group template
names. The only reason our Group model is called `BorrowdGroup` is to
avoid clashing with the Django core group model. We don't need to spell
it out everywhere.

This fixes a bug I introduced in a82b24e where, with the new
`BorrowdGroup` class name, the template lookup mixin no longer worked
correctly.

borrowd/util.py
@@ -27,5 +27,5 @@ class BorrowdTemplateFinderMixin:
 
     def get_template_names(self) -> list[str]:
         app_name = self.model._meta.app_label.replace("borrowd_", "")
-        model_name = self.model.__name__.lower()
+        model_name = self.model.__name__.lower().replace("borrowd", "")
         return [f"{app_name}/{model_name}{self.template_name_suffix}.html"]