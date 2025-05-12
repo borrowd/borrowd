1aeea75ea5e10f18f891c6c331f841b5cf64a286
Ryan Jendoubi <ryan.jendoubi@gmail.com>
2025-05-12 08:57:10 +0100

    Item search: add "security" / permission-based filtering.
    With tests.

--- borrowd_items/filters.py
+++ borrowd_items/filters.py
@@ -2,6 +2,7 @@ from typing import Any
 
 from django.db.models import Q, QuerySet
 from django_filters import CharFilter, FilterSet
+from guardian.shortcuts import get_objects_for_user
 
 from .models import Item
 
@@ -18,6 +19,34 @@ class ItemFilter(FilterSet):  # type: ignore[misc]
         return queryset.filter(
             Q(name__icontains=value) | Q(description__icontains=value)
         )
+
+    @property
+    def qs(self) -> QuerySet[Item]:
+        """
+        Override the qs property to filter the queryset based on user
+        permissions.
+
+        The overall structure of this method mirrors the caching
+        mechanism of the original that we're overriding here. The
+        main magic is using `django-guardian`'s `get_objects_for_user`
+        shortcut to filter the queryset based on the user's
+        permissions.
+
+        It's possible we could have achieved the same outcome by
+        setting the `queryset` attribute, but it was less clear
+        when this is accessed & updated; the `qs` property seemed
+        closer to a "public API".
+        """
+        if not hasattr(self, "_qs"):
+            qs: QuerySet[Item] = get_objects_for_user(
+                self.request.user, "view_this_item", klass=Item
+            )
+            if self.is_bound:
+                # ensure form validation before filtering
+                self.errors
+                qs = self.filter_queryset(qs)
+            self._qs = qs
+        return self._qs
 
     class Meta:
         model = Item
