a1b0e9321c299774a45a033a90dd7d2d9d5f38ac
Ryan Jendoubi <ryan.jendoubi@gmail.com>
2025-05-14 01:16:09 +0100

    Groups: require at least one Moderator at all times.
    Requires checks when removing members _and_ when updating Memberships
    to remove `is_moderator` status.

--- borrowd_groups/exceptions.py
+++ borrowd_groups/exceptions.py
@@ -3,3 +3,7 @@ from borrowd.exceptions import BorrowdException
 
 class ExistingMemberException(BorrowdException):
     pass
+
+
+class ModeratorRequiredException(BorrowdException):
+    pass
--- borrowd_groups/signals.py
+++ borrowd_groups/signals.py
@@ -1,8 +1,12 @@
-from django.db.models.signals import post_save
+from typing import Any
+
+from django.db.models.query import QuerySet
+from django.db.models.signals import post_save, pre_delete, pre_save
 from django.dispatch import receiver
 from guardian.shortcuts import assign_perm, remove_perm
 
 from borrowd.models import TrustLevel
+from borrowd_groups.exceptions import ModeratorRequiredException
 from borrowd_groups.models import BorrowdGroup, Membership
 from borrowd_items.models import Item
 from borrowd_users.models import BorrowdUser
@@ -35,6 +39,32 @@ def set_moderator_on_group_creation(
     )
 
 
+def _check_for_remaining_moderators(
+    user: BorrowdUser, group: BorrowdGroup, **kwargs: Any
+) -> None:
+    """
+    Check if a group has any remaining moderators.
+    If not, raise a ModeratorRequiredException.
+    """
+    # First, only apply this logic if we're NOT in a cascade delete
+    # from the Group itself.
+    origin = kwargs.get("origin")
+    if not origin or (
+        origin
+        and not isinstance(origin, BorrowdGroup)
+        and not (
+            isinstance(origin, QuerySet) and isinstance(origin.model, BorrowdGroup)
+        )
+    ):
+        other_moderators = Membership.objects.filter(
+            group=group, is_moderator=True
+        ).exclude(user=user)
+
+        if not other_moderators.exists():
+            # If there are no other moderators, assign the owner as a moderator
+            raise ModeratorRequiredException
+
+
 @receiver(post_save, sender=Membership)
 def refresh_item_permissions_on_membership_update(
     sender: Membership, instance: Membership, created: bool, **kwargs: str
@@ -59,3 +89,39 @@ def refresh_item_permissions_on_membership_update(
     for perm in ["view_this_item"]:  # will have more later
         remove_perm(perm, group, items_requiring_higher_trust)
         assign_perm(perm, group, items_requiring_lower_trust)
+
+
+@receiver(pre_delete, sender=Membership)
+def pre_membership_delete(
+    sender: Membership, instance: Membership, **kwargs: Any
+) -> None:
+    """
+    Remove all permissions for the user on the group and items
+    when their membership is deleted.
+    """
+    membership = instance
+    user: BorrowdUser = membership.user  # type: ignore[assignment]
+    group: BorrowdGroup = membership.group  # type: ignore[assignment]
+
+    #
+    # Check the group will not be left without a Moderator
+    #
+    _check_for_remaining_moderators(user, group, **kwargs)
+
+
+@receiver(pre_save, sender=Membership)
+def pre_membership_save(
+    sender: Membership, instance: Membership, **kwargs: Any
+) -> None:
+    """
+    Check if the user being saved is a moderator of the group.
+    If not, check if the group has any other moderators.
+    If not, raise a ModeratorRequiredException.
+    """
+    membership = instance
+    user: BorrowdUser = membership.user  # type: ignore[assignment]
+    group: BorrowdGroup = membership.group  # type: ignore[assignment]
+
+    # Check if the user is being added as a moderator
+    if not membership.is_moderator:
+        _check_for_remaining_moderators(user, group, **kwargs)
