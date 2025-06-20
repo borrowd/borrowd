929e133a3dc8ea3ac46f4060a6066522a99f750b
Ryan Jendoubi <ryan.jendoubi@gmail.com>
2025-06-20 16:52:01 +0100

    Include custom font in vite local bundling.

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,6 +3,28 @@ import { resolve } from 'path';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(({mode}) => {
+  /* I'm not sure whether this was a bug per se in vite or some
+  special interplay between vite and django, but there doesn't
+  seem to be a single value that can be used here that works for
+  both vite dev and vite build.
+
+  The issue seems to be the `url()` call used in the css to load in
+  the font file. If set to "fonts", then dev works, but build can't
+  find the target file. If set to "static/fonts" then build works,
+  but oddly in dev, the path of the file is translated to
+  "/static/static/fonts", which 404s.
+
+  Hence, after much struggling and cursing, simply using the values
+  here that work for each mode. */
+
+  let fontspath = "";
+  if (mode === "development") {
+    fontspath = "fonts";
+  }
+  else {
+    fontspath = "static/fonts";
+  }
+
   return {
     base: "/static/",
     build: {
@@ -18,6 +40,11 @@ export default defineConfig(({mode}) => {
     cacheDir: resolve("./node_modules/.vite"),
     plugins: [
       tailwindcss(),
-    ]
+    ],
+    resolve: {
+      alias: {
+        $fonts: resolve(fontspath),
+      }
+    }
   }
 });
