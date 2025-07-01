32544b55f51643df6921234210d4e46ac59166a8
Kevin Hines <krhines91@gmail.com>
2025-07-01 12:07:10 -0400

    Move django-vite app to dev settings

--- borrowd/config/dev/django.py
+++ borrowd/config/dev/django.py
@@ -1,11 +1,15 @@
 from ..base import *  # noqa: F403
+from ..base import BASE_DIR, INSTALLED_APPS  # explicitly import what we need
 
 DEBUG = True
 
-STATIC_ROOT = BASE_DIR / "staticfiles"  # noqa: F405
+STATIC_ROOT = BASE_DIR / "staticfiles"
 DJANGO_VITE = {
     "default": {
         "dev_mode": DEBUG,
         "manifest_path": BASE_DIR / "build" / "manifest.json",  # noqa: F405
     }
 }
+
+# Insert django_vite in second-to-last position of INSTALLED_APPS
+INSTALLED_APPS = INSTALLED_APPS[:-1] + ["django_vite"] + INSTALLED_APPS[-1:]
