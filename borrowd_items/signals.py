a120790c3ab6a64bb82a2a407fd095332e755583
bdebut01 <ben@debutts.com>
2025-07-31 09:51:22 -0400

    On Item trust level update, refresh item view permissions in all owner's groups

--- borrowd_items/signals.py
+++ borrowd_items/signals.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import Group
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from guardian.shortcuts import assign_perm
+from guardian.shortcuts import assign_perm, remove_perm
 
 from .models import Item
 
@@ -11,27 +11,34 @@ def assign_item_permissions(
     sender: Item, instance: Item, created: bool, **kwargs: str
 ) -> None:
     """
-    When a new Item is created, assign all relevant Item permissions
+    When a new Item is created or updated, assign all relevant Item permissions
     to the owner and relevant Groups.
     """
+    owner_borrowd_groups = instance.owner.borrowd_groups.all()  # type: ignore[attr-defined]
+    owner_groups = Group.objects.filter(
+        name__in=owner_borrowd_groups.values_list("name", flat=True)
+    )
+
     if created:
+        # For new items, assign owner permissions
         for perm in ["view_this_item", "edit_this_item", "delete_this_item"]:
             assign_perm(
                 perm,
                 instance.owner,
                 instance,
             )
+    else:
+        # For updated items, remove existing group permissions first
+        for group in owner_groups:
+            remove_perm("view_this_item", group, instance)
 
-        # Assign view permissions to all Groups of which the owner
-        # is a member and has an equal or greater Trust Level than
-        # the level required by this Item.
-        # mypy struggling a bit here again, even though intellisense
-        # knows that Item.owner (BorrowedUser) does indeed have a
-        # a Groups accessor.
-        allowed_borrowd_groups = instance.owner.borrowd_groups.filter(  # type: ignore[attr-defined]
-            membership__trust_level__gte=instance.trust_level_required
-        )
-        allowed_groups = Group.objects.filter(
-            name__in=allowed_borrowd_groups.values_list("name", flat=True)
-        )
-        assign_perm("view_this_item", allowed_groups, instance)
+    # Assign view permissions to all Groups of which the owner
+    # is a member and has an equal or greater Trust Level than
+    # the level required by this Item.
+    allowed_borrowd_groups = owner_borrowd_groups.filter(
+        membership__trust_level__gte=instance.trust_level_required
+    )
+    allowed_groups = owner_groups.filter(
+        name__in=allowed_borrowd_groups.values_list("name", flat=True)
+    )
+    assign_perm("view_this_item", allowed_groups, instance)
