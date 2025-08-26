borrowd/borrowd

commit 7b3ddcd5dafa28fa61c28661ea5c8be685eb3538

Group listing: proper group trust level.
This commit makes it so that one's proper Trust Level is reflected for
each Group on the Group listing page.

Note that, in order to do this, the generic list view and filter used
for that page are now based on the Membership model, _not_ the Group
model itself any longer. This is because the Trust Level is a property
of a User's _relationship_ with the Group, as captured in the Membership
through-model, rather than of the Group itself.

Note also that despite this change, the View class, Filter class and
template naming still refer to "Group" rather than switching everything
to "Membership". This reflects where one would expect to find these
components based on the app UI.

borrowd_groups/filters.py
@@ -2,9 +2,8 @@ from typing import Any
 
 from django.db.models import Q, QuerySet
 from django_filters import CharFilter, FilterSet
-from guardian.shortcuts import get_objects_for_user
 
-from .models import BorrowdGroup
+from .models import Membership
 
 
 # No typing for django_filter, so mypy doesn't like us subclassing.
@@ -12,16 +11,16 @@ class GroupFilter(FilterSet):  # type: ignore[misc]
     search = CharFilter(label="Search", method="filter_by_search")
 
     def filter_by_search(
-        self, queryset: QuerySet[BorrowdGroup], name: str, value: Any
-    ) -> QuerySet[BorrowdGroup]:
+        self, queryset: QuerySet[Membership], name: str, value: Any
+    ) -> QuerySet[Membership]:
         if not value:
             return queryset
         return queryset.filter(
-            Q(name__icontains=value) | Q(description__icontains=value)
+            Q(group__name__icontains=value) | Q(group__description__icontains=value)
         )
 
     @property
-    def qs(self) -> QuerySet[BorrowdGroup]:
+    def qs(self) -> QuerySet[Membership]:
         """
         Override the qs property to filter the queryset based on user
         permissions.
@@ -38,11 +37,10 @@ class GroupFilter(FilterSet):  # type: ignore[misc]
         closer to a "public API".
         """
         if not hasattr(self, "_qs"):
-            qs: QuerySet[BorrowdGroup] = get_objects_for_user(
-                self.request.user,
-                "view_this_group",
-                klass=BorrowdGroup,
-                with_superuser=False,
+            qs: QuerySet[Membership] = Membership.objects.select_related(
+                "group"
+            ).filter(
+                user=self.request.user,
             )
             if self.is_bound:
                 # ensure form validation before filtering
@@ -52,5 +50,5 @@ class GroupFilter(FilterSet):  # type: ignore[misc]
         return self._qs
 
     class Meta:
-        model = BorrowdGroup
+        model = Membership
         fields = ["search"]

borrowd_groups/views.py
@@ -229,9 +229,9 @@ class GroupJoinView(LoginRequiredMixin, View):
 
 
 # No typing for django_filter, so mypy doesn't like us subclassing.
-class GroupListView(BorrowdTemplateFinderMixin, FilterView):  # type: ignore[misc]
-    model = BorrowdGroup
-    template_name_suffix = "_list"  # Reusing template from ListView
+class GroupListView(FilterView):  # type: ignore[misc]
+    template_name = "groups/group_list.html"
+    model = Membership
     filterset_class = GroupFilter
 
 

templates/components/trust_pill.html
@@ -1,0 +1,27 @@
+{% if level|lower == 'high' %}
+<span class="px-2 inline-flex text-xs
+    leading-5 font-semibold rounded-full
+    bg-borrowd-fern-300
+    text-borrowd-fern-600">
+{% elif level|lower == 'medium' %}
+<span class="px-2 inline-flex text-xs
+    leading-5 font-semibold rounded-full
+    bg-borrowd-honey-300
+    text-borrowd-honey-600">
+{% elif level|lower == 'low' %}
+<span class="px-2 inline-flex text-xs
+    leading-5 font-semibold rounded-full
+    bg-borrowd-plum-300
+    text-borrowd-plum-600">
+{% else %}
+<span class="px-2 inline-flex text-xs
+    leading-5 font-semibold rounded-full
+    bg-borrowd-indigo-300
+    text-borrowd-indigo-600">
+{% endif %}
+    {% if slot %}
+        {{ slot }}
+    {% else %}
+        {{ level }}
+    {% endif %}
+</span>

templates/groups/group_list.html
@@ -51,30 +51,32 @@
                             </tr>
                         </thead>
                         <tbody class="bg-white divide-y divide-gray-200">
-                            {% for group in borrowdgroup_list %}
+                            {% for membership in object_list %}
                                 <tr>
                                     <td class="px-6 py-4 whitespace-nowrap">
                                         <div class="flex items-center">
-                                            {% if group.logo %}
+                                            {% if membership.group.logo %}
                                                 <div class="flex-shrink-0 h-10 w-10">
-                                                    <img class="h-10 w-10 rounded-full object-cover" src="{{ group.logo.url }}" alt="{{ group.name }} Logo">
+                                                    <img class="h-10 w-10 rounded-full object-cover"
+                                                        src="{{ membership.group.logo.url }}"
+                                                        alt="{{ membership.group.name }} Logo">
                                                 </div>
                                             {% endif %}
                                             <div class="ml-4">
                                                 <div class="text-sm font-medium text-gray-900">
-                                                    <a class="hover:underline text-blue-800" href="{% url 'borrowd_groups:group-detail' group.pk %}">{{ group.name }}</a>
+                                                    <a class="hover:underline text-blue-800"
+                                                        href="{% url 'borrowd_groups:group-detail' membership.group.pk %}">
+                                                        {{ membership.group.name }}
+                                                    </a>
                                                 </div>
                                             </div>
                                         </div>
                                     </td>
                                     <td class="px-6 py-4 whitespace-nowrap">
-                                        {# TODO: This is hardcoded for now, needs to be updated to display the actual trust level #}
-                                        <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-blue-100 text-blue-800">
-                                            High
-                                        </span>
+                                        <c-trust-pill level="{{ membership.get_trust_level_display }}" />
                                     </td>
                                     <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                        {{ group.users.count }}
+                                        {{ membership.group.users.count }}
                                     </td>
                                 </tr>
                             {% empty %}

commit 5f102f0915d7963eb9125392f338a32b27764faa

Fix GroupListViewVisibilityTests.
Breakage introduced in 7b3ddcd5dafa2.

borrowd_groups/tests/test_group_visibility_in_list_view.py
@@ -1,7 +1,7 @@
 from django.test import RequestFactory, TestCase
 
 from borrowd.models import TrustLevel
-from borrowd_groups.models import BorrowdGroup
+from borrowd_groups.models import BorrowdGroup, Membership
 from borrowd_groups.views import GroupListView
 from borrowd_users.models import BorrowdUser
 
@@ -29,7 +29,7 @@ class GroupListViewVisibilityTests(TestCase):
         owner = self.owner
 
         ## Create Group
-        group = BorrowdGroup.objects.create(
+        BorrowdGroup.objects.create(
             name="Test Group",
             created_by=owner,
             updated_by=owner,
@@ -44,13 +44,16 @@ class GroupListViewVisibilityTests(TestCase):
         # Act
         #
         response = GroupListView.as_view()(request)
-        groups = response.context_data["object_list"]
+        page_memberships = response.context_data["object_list"]
+
+        user_memberships = Membership.objects.filter(user=owner)
 
         #
         #  Assert
         #
-        self.assertEqual(len(groups), 1)
-        self.assertIn(group, groups)
+        self.assertEqual(len(page_memberships), 1)
+        self.assertEqual(len(user_memberships), 1)
+        self.assertQuerySetEqual(user_memberships, page_memberships)
 
     def test_group_member_can_list_group(self) -> None:
         """
@@ -82,13 +85,16 @@ class GroupListViewVisibilityTests(TestCase):
         # Act
         #
         response = GroupListView.as_view()(request)
-        groups = response.context_data["object_list"]
+        page_memberships = response.context_data["object_list"]
+
+        user_memberships = Membership.objects.filter(user=member)
 
         #
         #  Assert
         #
-        self.assertEqual(len(groups), 1)
-        self.assertIn(group, groups)
+        self.assertEqual(len(page_memberships), 1)
+        self.assertEqual(len(user_memberships), 1)
+        self.assertQuerySetEqual(user_memberships, page_memberships)
 
     def test_cannot_list_group_where_not_a_member(self) -> None:
         """
@@ -149,7 +155,7 @@ class GroupListViewVisibilityTests(TestCase):
         group.add_user(member, trust_level=TrustLevel.LOW)
 
         ## Preare the request
-        request = self.factory.get(f"/groups/{group.id}/")
+        request = self.factory.get(f"/groups/{group.pk}/")
         request.user = member
 
         #