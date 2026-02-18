7eaad0e0384d7157133c5c6b189a754df038b533
vguzman812 <vguzman812@gmail.com>
2026-02-18 18:57:35 +1100

    style(items): replace tailwind theme borrowd-* colors with daisyui theme colors

--- borrowd_items/card_helpers.py
+++ borrowd_items/card_helpers.py
@@ -23,12 +23,11 @@ BANNER_ICONS = {
     "borrowed": '<svg class="w-4 h-4" fill="currentColor" viewBox="0 0 24 24"><path fill-rule="evenodd" d="M7.5 6v.75H5.513c-.96 0-1.764.724-1.865 1.679l-1.263 12A1.875 1.875 0 004.25 22.5h15.5a1.875 1.875 0 001.865-2.071l-1.263-12a1.875 1.875 0 00-1.865-1.679H16.5V6a4.5 4.5 0 10-9 0zM12 3a3 3 0 00-3 3v.75h6V6a3 3 0 00-3-3zm-3 8.25a3 3 0 106 0v-.75a.75.75 0 011.5 0v.75a4.5 4.5 0 11-9 0v-.75a.75.75 0 011.5 0v.75z" clip-rule="evenodd"/></svg>',
 }
 
-# TODO: convert to Daisy themed classes
 BANNER_STYLES = {
-    "available": {"bg": "bg-borrowd-fern-300", "text": "text-borrowd-fern-600"},
-    "requested": {"bg": "bg-borrowd-indigo-300", "text": "text-borrowd-indigo-600"},
-    "reserved": {"bg": "bg-borrowd-plum-300", "text": "text-borrowd-plum-600"},
-    "borrowed": {"bg": "bg-borrowd-indigo-300", "text": "text-borrowd-indigo-600"},
+    "available": {"bg": "bg-success/15", "text": "text-success"},
+    "requested": {"bg": "bg-primary/15", "text": "text-primary"},
+    "reserved": {"bg": "bg-secondary/15", "text": "text-secondary"},
+    "borrowed": {"bg": "bg-primary/15", "text": "text-primary"},
 }
 
 
