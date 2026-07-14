4a3913d63fd7b7c71fbf92ff84148c446784e101
leila <leilaloezer@gmail.com>
2026-07-14 12:59:34 -0600

    Cleanup onboarding

--- borrowd_web/urls.py
+++ borrowd_web/urls.py
@@ -5,13 +5,11 @@ from .views import (
     onboarding_complete,
     onboarding_step1,
     onboarding_step2,
-    onboarding_step3,
 )
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("onboarding/1/", onboarding_step1, name="onboarding_step1"),
     path("onboarding/2/", onboarding_step2, name="onboarding_step2"),
-    path("onboarding/3/", onboarding_step3, name="onboarding_step3"),
     path("onboarding/complete/", onboarding_complete, name="onboarding_complete"),
 ]
