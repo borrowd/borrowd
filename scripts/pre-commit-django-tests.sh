ce19d495def0e546b098c99d894584cb57af0782
Pejjp <jeffpendy33@gmail.com>
2026-07-21 02:02:04 -0400

    Revert "probe falls back cleanly"
    This reverts commit 907e4de4ad5ab878ddde4d3e2a64816204971075.

--- scripts/pre-commit-django-tests.sh
+++ scripts/pre-commit-django-tests.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 
-if (: > /dev/tty) 2> /dev/null; then
+if [ -w /dev/tty ]; then
     printf "\n" > /dev/tty
     uv run manage.py test --verbosity 1 > /dev/tty 2>&1
     printf "\n" > /dev/tty
